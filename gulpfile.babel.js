ae68a69b0b7af88da8d44314631b8befafc8feaa
Nuttapat <nuttapat.kirawittaya@gmail.com>
2017-08-13 23:41:19 +0700

    Debug travis.yml

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,7 @@ import eslint from 'gulp-eslint';
 
 /**
  * Eslint our code base and fail on error
- **/
+ * */
 function lint() {
   return gulp.src(['src/*.js']).pipe(eslint()).pipe(eslint.formatEach()).pipe(eslint.failAfterError());
 }
