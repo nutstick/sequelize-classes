ce6f527f7958d39755497f7e76284df9924021e9
Nuttapat <nuttapat.kirawittaya@gmail.com>
2017-08-14 23:37:06 +0700

    Update model for sequelize v4

--- src/model.js
+++ src/model.js
@@ -172,15 +172,23 @@ export class Model {
    * @returns {Model}
    */
   registerModel(sequelize) {
-    const model = sequelize.define(this.constructor.name, this._fields, {
-      instanceMethods: this._instanceMethods,
-      indexes: this._indexes,
-      classMethods: this._classMethods,
-      getterMethods: this._getterMethods,
-      setterMethods: this._setterMethods,
-      validate: this._validate,
-      ...this.constructor._options
+    const self = this;
+
+    const model = sequelize.define(self.constructor.name, self._fields, {
+      indexes: self._indexes,
+      getterMethods: self._getterMethods,
+      setterMethods: self._setterMethods,
+      validate: self._validate,
+      ...self.constructor._options
     });
+
+    Object.keys(self._classMethods).forEach((key) => {
+      model[key] = self._classMethods[key];
+    }, self);
+
+    Object.keys(self._instanceMethods).forEach((key) => {
+      model.prototype[key] = self._instanceMethods[key];
+    }, self);
 
     this.declareHooks(model);
     // this.declareRelations( model, sequelize );
